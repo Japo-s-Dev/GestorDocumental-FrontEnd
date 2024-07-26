3ef2d4318526cd45e4a54bbe4e11534f7f4fc26e
Andre Marroquin <mar22266@uvg.edu.gt>
2024-07-25 19:56:16 -0600

    Feat: Home page created and progress bar

--- src/app/components/home-portal/home-portal.component.ts
+++ src/app/components/home-portal/home-portal.component.ts
@@ -3,9 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home-portal',
   templateUrl: './home-portal.component.html',
-  styleUrl: './home-portal.component.css'
+  styleUrls: ['./home-portal.component.css']
 })
 export class HomePortalComponent {
+  usedStorage: number = 600; // Storage in GB
+  totalStorage: number = 2000; // Total Storage in GB
 
+  getStoragePercentage(): number {
+    return (this.usedStorage / this.totalStorage) * 100;
+  }
 }
-//pribando
