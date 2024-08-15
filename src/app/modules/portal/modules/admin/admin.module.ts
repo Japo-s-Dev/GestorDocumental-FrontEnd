239988a98466d3b4bff7e5f45775236d113d70b0
SergioAle210 <ore221122@uvg.edu.gt>
2024-08-15 12:35:28 -0600

    FEAT: implement Translate module in users page

--- src/app/modules/portal/modules/admin/admin.module.ts
+++ src/app/modules/portal/modules/admin/admin.module.ts
@@ -6,7 +6,14 @@ import { UsersComponent } from './components/users/users.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { RolesComponent } from './components/roles/roles.component';
 import { FormsModule } from '@angular/forms';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
+// Función necesaria para cargar los archivos de traducción
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -17,7 +24,14 @@ import { FormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     FormsModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    TranslateModule.forChild({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ]
 })
 export class AdminModule { }
