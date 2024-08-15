6988fa759dfb3e09f5a0cea41cd4b98b7ab7fcf1
SergioAle210 <ore221122@uvg.edu.gt>
2024-08-15 12:25:55 -0600

    FEAT: implement language switching in header and add language toggle functionality

--- src/app/core/route-labels.ts
+++ src/app/core/route-labels.ts
@@ -1,11 +1,12 @@
 export const RouteLabels: { [key: string]: string } = {
-  '/portal': 'Home',
-  '/portal/search': 'Search > Criteria',
-  '/portal/search/result': 'Search > Result',
-  '/portal/add': 'Add',
-  '/portal/add/file-entry': 'Add > File Entry',
-  '/portal/admin': 'Admin',
-  '/portal/admin/users': 'Admin > Users',
-  '/portal/admin/projects': 'Admin > Projects',
-  '/portal/admin/roles': 'Admin > Roles'
+  '/portal': 'route:home',
+  '/portal/search': 'route:search_criteria',
+  '/portal/search/result': 'route:search_result',
+  '/portal/add': 'route:add',
+  '/portal/add/file-entry': 'route:add_file_entry',
+  '/portal/add/upload-documents': 'route:upload-documents',
+  '/portal/admin': 'route:admin',
+  '/portal/admin/users': 'route:admin_users',
+  '/portal/admin/projects': 'route:admin_projects',
+  '/portal/admin/roles': 'route:admin_roles'
 };
