55ee5b2bc4f4a1793aadca1d6e120b6144d16985
SergioAle210 <ore221122@uvg.edu.gt>
2024-08-15 12:29:27 -0600

    FEAT: implement language switching in home portal and add language toggle functionality

--- src/app/components/home-portal/home-portal.component.ts
+++ src/app/components/home-portal/home-portal.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home-portal',
@@ -8,8 +9,19 @@ import { Component } from '@angular/core';
 export class HomePortalComponent {
   usedStorage: number = 600; // Storage in GB
   totalStorage: number = 2000; // Total Storage in GB
+  currentLanguage: string;
+
+  constructor(private translate: TranslateService) {
+    this.currentLanguage = this.translate.getDefaultLang() || 'es';
+    this.translate.setDefaultLang(this.currentLanguage);
+  }
 
   getStoragePercentage(): number {
     return (this.usedStorage / this.totalStorage) * 100;
+  }
+
+  switchLanguage(language: string) {
+    this.translate.use(language);
+    this.currentLanguage = language;
   }
 }
