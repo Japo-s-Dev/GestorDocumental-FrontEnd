421c04b59589fa0f861ce52c0cf549935decf69a
SergioAle210 <ore221122@uvg.edu.gt>
2024-08-15 14:28:08 -0600

    FEAT: implement view for uploading documents

---
+++ src/app/modules/portal/modules/add-file/components/upload-document/upload-document.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-upload-document',
+  templateUrl: './upload-document.component.html',
+  styleUrls: ['./upload-document.component.css']
+})
+export class UploadDocumentComponent implements OnInit {
+  selectedFile: File | null = null;
+  fileUploaded = false;
+  filePreviewUrl: string | null = null;  // Asegúrate de que sea de tipo string o null
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    // Asignar una URL de prueba al filePreviewUrl
+    this.filePreviewUrl = 'https://pfafner.github.io/tc2024/aulas/Aula05.pdf';
+  }
+
+  onFileSelected(event: any): void {
+    const file = event.target.files[0];
+    if (file) {
+      this.selectedFile = file;
+      this.fileUploaded = true;
+
+      const reader = new FileReader();
+      reader.onload = () => {
+        // Asegúrate de que filePreviewUrl sea siempre una cadena
+        if (typeof reader.result === 'string') {
+          this.filePreviewUrl = reader.result;
+        } else {
+          this.filePreviewUrl = null;
+        }
+      };
+      
+      reader.readAsDataURL(file);
+    }
+  }
+
+  saveDocument(): void {
+    if (this.selectedFile) {
+      localStorage.setItem('uploadedFile', this.selectedFile.name);
+      this.router.navigate(['/portal/add/file-entry']);
+    }
+  }
+}
