ecbfb379c1c1cb7cb4afebffe577e322b4473580
SergioAle210 <ore221122@uvg.edu.gt>
2024-09-01 12:31:48 -0600

    FEAT: Se implementaron todas las pruebas unitarias para el componente del home-portal

--- src/app/components/home-portal/home-portal.component.spec.ts
+++ src/app/components/home-portal/home-portal.component.spec.ts
@@ -1,17 +1,47 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, fakeAsync, tick, waitForAsync } from '@angular/core/testing';
 import { HomePortalComponent } from './home-portal.component';
+import { TranslateModule, TranslateService, TranslateLoader, LangChangeEvent } from '@ngx-translate/core';
+import { CUSTOM_ELEMENTS_SCHEMA, EventEmitter } from '@angular/core';
+import { of } from 'rxjs';
+import { LoaderService } from '../../services/loader.service';
+import { TranslateFakeLoader } from '@ngx-translate/core';
 
 describe('HomePortalComponent', () => {
   let component: HomePortalComponent;
+  let loaderServiceSpy: jasmine.SpyObj<LoaderService>;
   let fixture: ComponentFixture<HomePortalComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [HomePortalComponent]
+  const translateServiceMock = {
+    currentLang: 'en',
+    onLangChange: new EventEmitter<LangChangeEvent>(),
+    use: jasmine.createSpy('use').and.returnValue(of('')),
+    get: jasmine.createSpy('get').and.returnValue(of('')),
+    getDefaultLang: jasmine.createSpy('getDefaultLang').and.returnValue('en'), // Simula getDefaultLang,
+    setDefaultLang: jasmine.createSpy('getDefaultLang').and.returnValue('es'), // Simula getDefaultLang
+    onTranslationChange: new EventEmitter(),
+    onDefaultLangChange: new EventEmitter(),
+  };
+
+  beforeEach(waitForAsync(() => {
+    loaderServiceSpy = jasmine.createSpyObj('LoaderService', ['hideLoader']);
+
+    TestBed.configureTestingModule({
+      declarations: [ HomePortalComponent ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [
+        TranslateModule.forRoot({
+          loader: { provide: TranslateLoader, useClass: TranslateFakeLoader } // Cargador falso
+        })
+      ],
+      providers: [
+        { provide: LoaderService, useValue: loaderServiceSpy },
+        { provide: TranslateService, useValue: translateServiceMock }
+      ]
     })
     .compileComponents();
-    
+  }));
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(HomePortalComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +49,24 @@ describe('HomePortalComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should hide the loader after 2 seconds in ngOnInit', fakeAsync(() => {
+    component.ngOnInit();
+    tick(2000);
+    expect(loaderServiceSpy.hideLoader).toHaveBeenCalled();
+  }));
+
+  it('should return correct storage percentage', () => {
+    const percentage = component.getStoragePercentage();
+    expect(percentage).toBeCloseTo(30, 0);
+  });
+
+  it('should switch language and update currentLanguage', () => {
+    const newLanguage = 'en';
+    component.switchLanguage(newLanguage);
+    fixture.detectChanges();
+    expect(translateServiceMock.use).toHaveBeenCalledWith(newLanguage);
+    expect(component.currentLanguage).toBe(newLanguage);
   });
 });
