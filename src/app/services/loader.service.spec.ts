44fd54a9b6fabe4ebf62c6df25c0be6d07adba2a
SergioAle210 <ore221122@uvg.edu.gt>
2024-09-26 23:31:32 -0600

    FEAT: Se agregaron las pruebas para el servicio del loader

--- src/app/services/loader.service.spec.ts
+++ src/app/services/loader.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { LoaderService } from './loader.service';
 
 describe('LoaderService', () => {
@@ -12,5 +11,23 @@ describe('LoaderService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should emit true when showLoader is called', (done) => {
+    service.loaderState$.subscribe((state) => {
+      expect(state).toBeTrue();
+      done(); // Completa la prueba cuando se reciba el valor
+    });
+
+    service.showLoader();
+  });
+
+  it('should emit false when hideLoader is called', (done) => {
+    service.loaderState$.subscribe((state) => {
+      expect(state).toBeFalse();
+      done(); // Completa la prueba cuando se reciba el valor
+    });
+
+    service.hideLoader();
   });
 });
