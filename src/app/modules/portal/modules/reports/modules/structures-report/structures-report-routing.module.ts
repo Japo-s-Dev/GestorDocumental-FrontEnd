11b84670d4f0e58957b5c8a8569a83c225c1bc47
SergioAle210 <ore221122@uvg.edu.gt>
2024-10-04 15:09:43 -0600

    FEAT: Se agrego la ruta para la reporteria de estructuras

--- src/app/modules/portal/modules/reports/modules/structures-report/structures-report-routing.module.ts
+++ src/app/modules/portal/modules/reports/modules/structures-report/structures-report-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StructuresReportComponent } from './components/structures-report/structures-report.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: StructuresReportComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
