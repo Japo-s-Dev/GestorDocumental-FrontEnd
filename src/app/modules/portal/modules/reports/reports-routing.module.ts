212a074c2a47e71ecae8d0683141d42ac4952ece
SergioAle210 <ore221122@uvg.edu.gt>
2024-10-04 14:10:30 -0600

    FEAT: Se agregaron las rutas necesarias para la reportería

--- src/app/modules/portal/modules/reports/reports-routing.module.ts
+++ src/app/modules/portal/modules/reports/reports-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'actions-report',
+    loadChildren: () => import('./modules/actions-report/actions-report.module').then(m => m.ActionsReportModule)
+  },
+  {
+    path: 'structures-report',
+    loadChildren: () => import('./modules/structures-report/structures-report.module').then(m => m.StructuresReportModule)
+  },
+  {
+    path: 'user-report',
+    loadChildren: () => import('./modules/user-report/user-report.module').then(m => m.UserReportModule)
+  },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
