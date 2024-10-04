a994b23a5ea3e94ba934747d91de72a9670fe1d4
SergioAle210 <ore221122@uvg.edu.gt>
2024-10-04 14:16:57 -0600

    FEAT: Se implementó para el cambió de traducción en la reportería

--- src/app/modules/portal/modules/reports/reports.module.ts
+++ src/app/modules/portal/modules/reports/reports.module.ts
@@ -6,6 +6,15 @@ import { ReportsRoutingModule } from './reports-routing.module';
 import { UserReportComponent } from './modules/user-report/components/user-report/user-report.component';
 import { ActionsReportComponent } from './modules/actions-report/components/actions-report/actions-report.component';
 import { StructuresReportComponent } from './modules/structures-report/components/structures-report/structures-report.component';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+// Función necesaria para cargar los archivos de traducción
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
 
 @NgModule({
   declarations: [
@@ -15,7 +24,14 @@ import { StructuresReportComponent } from './modules/structures-report/component
   ],
   imports: [
     CommonModule,
-    ReportsRoutingModule
+    ReportsRoutingModule,
+    TranslateModule.forChild({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ]
 })
 export class ReportsModule { }
