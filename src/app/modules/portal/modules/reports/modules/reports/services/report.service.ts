a7bf92c698d2a0245d2307b1a0f5e26e9f75ca51
SergioAle210 <ore221122@uvg.edu.gt>
2024-10-31 12:11:39 -0600

    FEAT: Se implementó la función de eventos para la reportería

---
+++ src/app/modules/portal/modules/reports/modules/reports/services/report.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AppConstants } from '../../../../../../../enums/app.constants';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ReportService {
+  private apiUrl = `${AppConstants.BASE_URL}/api/rpc`;
+
+  constructor(private http: HttpClient) {}
+
+  listEvents(): Observable<any> {
+    const payload = {
+      id: 1,
+      method: 'list_events',
+      params: {
+        filters: {},
+        list_options: {
+          order_bys: 'user_id',
+        },
+      },
+    };
+    
+    const formData = this.createFormData(payload);
+    return this.http.post<any>(this.apiUrl, formData, {
+      observe: 'response',
+      withCredentials: true,
+    });
+  }
+
+  private createFormData(payload: any): FormData {
+    const formData = new FormData();
+    formData.append('json', JSON.stringify(payload));
+    return formData;
+  }
+}
